a59a2d2b026819d25bfc8f0abf737358262f4b30
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-07 15:46:43 +0530

    word.c

---
+++ word.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main()
+{
+int a[20];
+printf("\n Enter the number");
+scanf("%d",&a);
+switch(a)
+{
+    case 1:
+    printf("\n one");
+    break;
+    case 2:
+    printf("\n two");
+    break;
+    case 3:
+    printf("\n three"); 
+    break;
+    case 4:
+    printf("\n four");
+    break;
+    case 5: 
+    printf("\n five");
+    break;
+    case 6: 
+    printf("\n six");
+    break;
+    case 7:
+    printf("\n seven");
+    break;
+    case 8:
+    printf("\n eight"); 
+    break;
+    case 9:
+    printf("\n nine");
+    break; 
+    default:
+    printf("\n enter single digit number");
+    break;
+}
+}

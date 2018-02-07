7f8c3149ad26728d3d4bdbb850678a77dfd88c15
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-07 16:10:14 +0530

    bit.c

---
+++ bit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i=50;
+a=500;
+printf("Value of i=%d a=%d before swap",i,a);
+i=i^a;
+k=i^a;
+i=i^a;
+printf("value of i=%d a=%d after swap ",i,a);
+return 0;
+}
